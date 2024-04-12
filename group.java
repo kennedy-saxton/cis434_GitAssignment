5b689958f61d78d51056f1a4fb000e7f98d4a193
CIRE-3 <118152330+CIRE-3@users.noreply.github.com>
2024-04-12 13:53:31 -0400

    Update group.java

--- group.java
+++ group.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 
 public class Group {
 
@@ -12,6 +13,14 @@ public class Group {
     public static void getGoupMembers(){
         System.out.println("Eric Hernandez");
         
+    }
+
+    public static int power(int num1,int num2){
+        num1 = 2;
+        num2 = 3;
+        double x;
+         x = Math.pow(num1, num2);
+        return (int) x;
     }
 }
 
