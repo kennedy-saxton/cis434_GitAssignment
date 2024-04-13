f1e2aad13d7910f169ba80400aae3a88409f4953
Imanyy28 <162731123+Imanyy28@users.noreply.github.com>
2024-04-12 23:58:49 -0400

    Update group.java

--- group.java
+++ group.java
@@ -12,6 +12,21 @@ public class Group {
     public static void getGoupMembers(){
         System.out.println("Eric Hernandez");
         
+    
+// code idea from https://www.javatpoint.com/power-of-a-number-in-java
+    public static int power(int num1,int num2){
+        num1 = 2;
+        num2 = 3;
+        double x;
+        for(int i = 0; i < num2; i++){
+            x *= num1;
+    }
+        return (int) x;
+}
+
+     public static void main(String[] args) {
+        addE();
+    }
     }
    public static void addE(){
         System.out.println("Imane Lamine");
@@ -32,21 +47,6 @@ public class Group {
         }
         return sign * total;
     }
-// code idea from https://www.javatpoint.com/power-of-a-number-in-java
-    public static int power(int num1,int num2){
-        num1 = 2;
-        num2 = 3;
-        double x;
-        for(int i = 0; i < num2; i++){
-            x *= num1;
-    }
-        return (int) x;
-}
-
-     public static void main(String[] args) {
-        addE();
-    }
-    
     public static void addE(){
         System.out.println("Hasn Hameed");
     }
