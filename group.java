25091a38c6a288311b7165662d1ea517e45dceed
Imanyy28 <162731123+Imanyy28@users.noreply.github.com>
2024-04-23 20:59:22 -0400

    Update group.java

--- group.java
+++ group.java
@@ -28,24 +28,18 @@ public class Group {
         addE();
     }
     }
-   public static void addE(){
-        System.out.println("Imane Lamine");
+  // code idea from https://www.javatpoint.com/power-of-a-number-in-java
+    public static int power(int num1,int num2){
+        num1 = 2;
+        num2 = 3;
+        double x = 0;
+        for(int i = 0; i < num2; i++){
+            x = num1;
     }
-
-    public static void getGoupMembers(){
+        return (int) x;
+}
+   public static void addI(){
         System.out.println("Imane Lamine");
-        
-    }
-// Method to perform multiplication using addition
-    public int multiply(int num1, int num2) {
-        int total = 0;
-        int sign = (num1 < 0) ^ (num2 < 0) ? -1 : 1;
-        num1 = Math.abs(num1);
-        num2 = Math.abs(num2);
-        for (int i = 0; i < num1; i++) {
-            total += num2;
-        }
-        return sign * total;
     }
     public static void addE(){
         System.out.println("Hasn Hameed");
